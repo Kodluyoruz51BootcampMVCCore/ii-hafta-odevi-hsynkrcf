cb108a92e665143504f2234b93fc60d10842914f
Hüseyin Karacif <hsynkrcf@gmail.com>
2020-07-04 01:31:16 +0300

    Auth Örnekleri Oluşturuldu
    Denendi ve onaylandı :)

---
+++ Work or School Accounts Auth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Work_or_School_Accounts_Auth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
